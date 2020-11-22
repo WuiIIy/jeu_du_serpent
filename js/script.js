8efac2ed90fa5d51e53c55b3204e70fae28a3429
despatie william <williamdbonneville@hotmail.com>
2020-11-22 12:02:59 -0500

    le prof y voulait que je fasse un comit et un push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,79 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    //le jeu
+    class Jeu{
+
+        constructor(_idSvg, _idPointage) {
+
+            console.log("creation");
+
+            this.s = Snap(_idSvg)
+
+            this.sortiePointage = document.querySelector(_idPointage);
+
+            this.grandeurCarre = 20;
+
+            this.grandeurGrille = 15;
+
+        }
+
+        nouvellePartie(){
+
+        this.affichagePointage(1);
+
+        this.pomme = new Pomme();
+
+        this.serpent = new Serpent();
+
+        }
+
+        finPartie(){
+
+
+
+        }
+
+        affichagePointage(_lePointage){
+
+        this.sortiePointage.innerHTML = _lePointage;
+
+        }
+
+    }
+
+    //le serpent
+    class Serpent{
+
+        constructor() {
+
+            console.log("serpent")
+
+        }
+
+    }
+
+    //la pomme
+    class Pomme{
+
+        constructor() {
+
+            console.log("pomme")
+
+        }
+    }
+
+    var unePartie = new Jeu("#jeu", "#pointage");
+
+    var btnJouer = document.querySelector("#btnJouer");
+    btnJouer.addEventListener("click", nouvellePartie)
+
+    function nouvellePartie(){
+
+        unePartie.nouvellePartie();
+
+    }
+
+
 
 });
